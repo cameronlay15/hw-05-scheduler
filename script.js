93fbd78940040066038a7e57643fc0dfac619d40
Cameron Lay <cameronlay15@gmail.com>
2020-09-11 19:13:08 -0400

    got am/pm set up

--- script.js
+++ script.js
@@ -3,5 +3,23 @@
 $(document).ready(function(){
     $("#currentDay").text(moment().format("dddd, MMMM Do"));
 })
+for(i=9; i<18;i++){
+    
+
+   function amPm(hours) {
+    //    assigns pm/am based on if hours is greater than or equal to 12
+       var ampm= hours >= 12 ? 'PM' : 'AM';
+    //  sets hours back to 1 after 12
+       hours=hours%12;
+    // sets noon as 12
+       hours=hours ? hours : 12;
+    // returns if the hours are am or pm
+       return hours + ampm;
+   }
+amPm()
+}
+
+
+
 
 
