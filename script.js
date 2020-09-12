9d099b195211738da88b81970790b8f180f31a14
Cameron Lay <cameronlay15@gmail.com>
2020-09-12 10:32:02 -0400

    got the grid and coloring to work

--- script.js
+++ script.js
@@ -28,6 +28,39 @@ var planWorkday = [
 console.log(now)
 console.log(planWorkday)
 
+planWorkday.forEach(function(timeBlock, index) {
+	var timeLabel = timeBlock.time;
+	var blockColor = colorMe(timeLabel);
+
+	// variable containing the entire row, including the button
+	var row =
+		'<div class="time-block" id="' +
+		index +
+		'"><div class="row no-gutters input-group"><div class="col-sm col-lg-1 input-group-prepend hour justify-content-sm-end pr-3 pt-3">' +
+		timeLabel +
+		'</div><textarea class="form-control ' +
+		blockColor +
+		'">' +
+		timeBlock.event +
+		'</textarea><div class="col-sm col-lg-1 input-group-append"><button class="saveBtn btn-block" type="submit"><i class="fas fa-plus"></i></button></div></div></div>';
+
+	// show entry rows
+	$(".container-fluid").append(row);
+});
+
+// function for past, present, next color
+function colorMe(time) {
+	var planNow = moment(now, "H A");
+	var planEntry = moment(time, "H A");
+	if (planNow.isBefore(planEntry) === true) {
+		return "future";
+	} else if (planNow.isAfter(planEntry) === true) {
+		return "past";
+	} else {
+		return "present";
+	}
+}
+
 
 
 
