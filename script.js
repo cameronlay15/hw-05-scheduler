585905c05dd72ea91989b61eec4ec005ab2dcff8
Cameron Lay <cameronlay15@gmail.com>
2020-09-11 21:50:34 -0400

    got time to work

--- script.js
+++ script.js
@@ -18,7 +18,27 @@ for(i=9; i<18;i++){
    }
 amPm()
 }
+$(function() {});
 
+// today is the current day
+var today = moment().format("dddd, MMMM Do");
+
+// now is the current hour
+var now = moment().format("H A");
+
+// planWorkday entries for each hour of the workday
+var planWorkday = [
+	{ time: "9 AM", event: "" },
+	{ time: "10 AM", event: "" },
+	{ time: "11 AM", event: "" },
+	{ time: "12 PM", event: "" },
+	{ time: "1 PM", event: "" },
+	{ time: "2 PM", event: "" },
+	{ time: "3 PM", event: "" },
+	{ time: "4 PM", event: "" },
+	{ time: "5 PM", event: "" }
+];
+console.log(now)
 
 
 
